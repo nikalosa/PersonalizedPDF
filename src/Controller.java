b4e7e4a30793c959a6ba10c5e75cd15e9b164900
nikalosa <nlosa16@freeuni.edu.ge>
2019-01-10 17:28:21 +0400

    Everything you wish

--- src/Controller.java
+++ src/Controller.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 
 public class Controller {
     public static void main(String[] args) {
-        String txt = "";
+        /*String txt = "";
         for(int i=0;i<args[0].length();i++){
             txt += ""+args[0].charAt(i)+" ";
 
         }
-        String sourcePath = args[1];
+        txt = "   "+txt+"  ";
+        String sourcePath = args[1];*/
+        String txt = "NIKA";
+        String sourcePath = "Aries.pdf";
         PDFItext sol = new PDFItext();
         try {
             sol.manipulatePdf("D:\\Freelancer\\PersonalizedPDF\\Material\\"+sourcePath,
