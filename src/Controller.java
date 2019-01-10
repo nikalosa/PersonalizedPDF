f42292130bcfcfef0e167c19da5d6c99b122cd31
nikalosa <nlosa16@freeuni.edu.ge>
2019-01-10 16:42:56 +0400

    Everything you wish

--- src/Controller.java
+++ src/Controller.java
@@ -6,12 +6,14 @@ public class Controller {
     public static void main(String[] args) {
         String txt = "";
         for(int i=0;i<args[0].length();i++){
-            txt += ""+args[0].charAt(i)+' ';
+            txt += ""+args[0].charAt(i)+" ";
 
         }
+        String sourcePath = args[1];
         PDFItext sol = new PDFItext();
         try {
-            sol.manipulatePdf("D:\\Freelancer\\PDFJava\\src\\pdf.pdf", "D:\\Freelancer\\PDFJava\\src\\bla.pdf", txt);
+            sol.manipulatePdf("D:\\Freelancer\\PersonalizedPDF\\Material\\"+sourcePath,
+                    "D:\\Freelancer\\PersonalizedPDF\\Result\\Aries-"+txt+".pdf", txt);
 
         } catch (IOException e) {
             e.printStackTrace();
